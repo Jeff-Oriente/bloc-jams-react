a5c4cf43b3aab67a6d39ca93752ca39a8a3d3a99
jeff oriente <jdanieloriente@gmail.com>
2019-02-15 03:51:07 -0500

    added next button to playerbar

--- src/components/Album.js
+++ src/components/Album.js
@@ -56,6 +56,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(4, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   handleOnMouseEnter(song) {
     this.setState({hoveredSong: song});
   }
@@ -128,6 +136,7 @@ class Album extends Component {
       currentSong={this.state.currentSong}
       handleSongClick={() => this.handleSongClick(this.state.currentSong)}
       handlePrevClick={() => this.handlePrevClick()}
+      handleNextClick={() => this.handleNextClick()}
       />
       </section>
     );
